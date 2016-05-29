e19fbd541b1aa7f44cb15b653189d82adc155f7d
xinyuly <li_xinyuly@163.com>
2016-05-29 15:20:14 +0800

    1.初始化项目 2.添加Reachability

---
+++ XYNetworkStatus/XYNetworkStatus/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XYNetworkStatus
+//
+//  Created by lixinyu on 16/5/29.
+//  Copyright © 2016年 xiaoyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
